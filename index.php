8a29edc8837e78026dacfe0cd07e154d3450218e
root <ngomfa@gmail.com>
2018-11-20 16:05:27 +0100

    exo_4

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie3_Exo4</title>
+</head>
+<body>
+    <?php
+/*Créer une variable et l'initialiser à 1.
+Tant que cette variable n'atteint pas 10, il faut :
+
+    l'afficher
+    l'incrementer de la moitié de sa valeur
+*/
+
+        $number = 1;
+          while($number <=10){   ?>
+
+            <p><?php echo $number = $number * 1.5;
+          }
+?></p>
+</body>
+</html>
